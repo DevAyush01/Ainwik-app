d8e48d2857a2961e147aaf0c31dab8036e9e2d0c
DevAyush01 <Ayushchoudharry00@gmail.com>
2024-10-08 16:35:48 +0530

    initial commit

--- server/multerConfig.js
+++ server/multerConfig.js
@@ -2,7 +2,7 @@ const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '.././uploads');
+const uploadDir = path.join(__dirname, '../uploads');
 
 
 // Ensure the uploads directory exists
